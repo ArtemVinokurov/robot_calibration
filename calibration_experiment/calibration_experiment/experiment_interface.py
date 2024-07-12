c7ba01e3259f4341600cca17e710629afba3dbee
ArtemVinokurov <vinokurov1768@yandex.ru>
2024-07-12 15:46:11 +0300

    update experiment interface

--- calibration_experiment/calibration_experiment/experiment_interface.py
+++ calibration_experiment/calibration_experiment/experiment_interface.py
@@ -12,6 +12,8 @@ class ClientNode(Node):
         super().__init__('experiment_interface')
         self.base_calibration_cli = self.create_client(Empty, "/base_frame_calibration")
         self.start_experiment_cli = self.create_client(Empty, "/start_experiment")
+        self.base_experiment_cli = self.create_client(Empty, "/start_base_experiment")
+        self.tool_experiment_cli = self.create_client(Empty, "/start_tool_experiment")
 
         while not self.base_calibration_cli.wait_for_service(timeout_sec=1.0) \
               or not self.start_experiment_cli.wait_for_service(timeout_sec=1.0):
@@ -25,6 +27,14 @@ class ClientNode(Node):
     def base_calibration_req(self):
         req = Empty.Request()
         return self.base_calibration_cli.call_async(req)
+    
+    def start_base_req(self):
+        req = Empty.Request()
+        return self.base_experiment_cli.call_async(req)
+    
+    def start_tool_req(self):
+        req = Empty.Request()
+        return self.tool_experiment_cli.call_async(req)
 
 
 def main():
@@ -34,14 +44,21 @@ def main():
 
     
     while rclpy.ok():
-        print("1. Calibration base frame \n2. Start Experiment")
+        print("1. Calibration base frame \n2. Base experiment\n3. Tool experiment\n4. Generally experiment")
         command = int(input())
         if command == 1:
             future = node.base_calibration_req()
             rclpy.spin_until_future_complete(node, future)
         elif command == 2:
+            future = node.start_base_req()
+            rclpy.spin_until_future_complete(node, future)
+        elif command == 3:
+            future = node.start_tool_req()
+            rclpy.spin_until_future_complete(node, future)
+        elif command == 4:
             future = node.start_experiments_req()
             rclpy.spin_until_future_complete(node, future)
+
 
     rclpy.shutdown()
 
